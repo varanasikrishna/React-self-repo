1a433f8bfa886fe4d48859eb23e2dcc90de7a02f
V D S Krishna <varanasi.kris@gmail.com>
2022-07-26 20:44:37 +0530

    Add my-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState({ userName: "", password: "" });
+  const { userName, password } = data;
+  const changeHandler = (e) =>
+    setData({ ...data, [e.target.name]: [e.target.value] });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            value={userName}
+            name="userName"
+            onChange={changeHandler}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            name="password"
+            onChange={changeHandler}
+          />
+          <br />
+          <br />
+          <input type="submit" value="submit" />
+        </form>
+      </center>
     </div>
   );
 }
-
-export default App;
